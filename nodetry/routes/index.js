d13cc5ea287785846254fe3df10be2a721bc1fe2
Shivalik <shvlksen@gmail.com>
2015-04-30 03:36:49 -0400

    Index.html and pad.html have the html,css,js code needed for dashboard, live charts and collab pad. Express routing might give problems, hence other links have been dummied out right now.

--- nodetry/routes/index.js
+++ nodetry/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. 127.0.0.1:5000/*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Collaboration yo!' });
+});
+
+/* GET Hello World page.i.e. 127.0.0.1:5000/helloworld? */
+router.get('/helloworld', function(req, res) {
+    res.render('thrifty', { title: 'GDFR' })
 });
 
 module.exports = router;
